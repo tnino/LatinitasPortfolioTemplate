8ca4633ecc207c52375421e57271e1efa3a867e6
Latinitas Austin <latinitasaustin@latinitass-mbp.attlocal.net>
2019-06-15 13:24:27 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.getElementById("name").innerHTML = "my name is Tatiana ";
+  }
+  function myFunction1() {
+    document.getElementById("school").innerHTML = "I went to UNLV ";
+  }
+  function myFunction2() {
+    document.getElementById("work").innerHTML = "I work at HomeAway ";
+  }
